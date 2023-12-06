89226d6bd3beabaa63fd41c2f44efb1a08a4abfd
Radoslav Bukov <radoslaw.bukow@gmail.com>
2023-12-06 18:45:23 +0200

    Properties Binding and view  encapsulation

--- src/app/btn-array-display/btn-array-display.component.ts
+++ src/app/btn-array-display/btn-array-display.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-btn-array-display',
   templateUrl: './btn-array-display.component.html',
-  styleUrl: './btn-array-display.component.css'
+  styleUrl: './btn-array-display.component.css',
+  encapsulation:ViewEncapsulation.Emulated,
 })
 export class BtnArrayDisplayComponent {
   showText = false;
   clickArray = [];
+  @Input() numberOfClicks = 0;
 
   changeDisplay() {
     this.clickArray.push(this.clickArray.length + 1);
+    this.numberOfClicks += 1;
     if (this.showText === true) {
       this.showText = !this.showText;
       return false;
